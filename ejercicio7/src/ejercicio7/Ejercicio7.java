d27b1513560dfe54e24916b7d758cc8a8881d0c7
HAJIMENOSAITO <103212000+HAJIMENOSAITO@users.noreply.github.com>
2022-04-14 23:35:00 -0400

    Add files via upload

---
+++ ejercicio7/src/ejercicio7/Ejercicio7.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio7;
+
+/**
+ *
+ * @author emil
+ */
+public class Ejercicio7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numero = 1;
+        
+        while(numero <= 100) {
+      System.out.println(numero);  // Se ejecuta 10 veces.
+      numero++;
+    }
+    
+}}
